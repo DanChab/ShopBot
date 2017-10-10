fde00430e357e7560ac3485045dba241f1ef5640
Danchab <banchabala@gmail.com>
2017-10-10 11:33:36 +0200

    TypeError: item.senderId is not a function bug FIXED

--- bot.js
+++ bot.js
@@ -111,20 +111,20 @@ var processMessage = (event) => {
     if (message.text) {
       var formattedMsg = message.text.toLowerCase().trim();
 
-       // Check if the json file contains an item to edit
-      // var note = jf.getNote(senderId);
-      // console.log('Json File here:', note);
-      // if(typeof note.senderId !== 'undefined'){
-      //   console.log("notes...",note);
-      //   var botMsg = _.get(note,'item.botMsg');
-      //   var itemId = _.get(note,'item.itemId');
-      //   var itemName = _.get(note,'item.itemName');
-      //   var itemPrice = _.get(note,'item.itemPrice');
-      //   console.log(botMsg);
-      //   // sanitise the user input so we only get the expected data type.
-      //   sanitise.inputValidator(senderId, itemId, itemName, itemPrice, botMsg, formattedMsg);
-      // }
-      // Get the first name of user from the fb graph api.
+       //Check if the json file contains an item to edit
+      var note = jf.getNote(senderId);
+      console.log('Json File here:', note);
+      if(typeof note.senderId !== 'undefined'){
+        console.log("notes...",note);
+        var botMsg = _.get(note,'item.botMsg');
+        var itemId = _.get(note,'item.itemId');
+        var itemName = _.get(note,'item.itemName');
+        var itemPrice = _.get(note,'item.itemPrice');
+        console.log(botMsg);
+        // sanitise the user input so we only get the expected data type.
+        sanitise.inputValidator(senderId, itemId, itemName, itemPrice, botMsg, formattedMsg);
+      }
+      //Get the first name of user from the fb graph api.
       request({
         url: "https://graph.facebook.com/v2.6/" + senderId,
         qs: {
@@ -211,7 +211,7 @@ var processMessage = (event) => {
       }
     });
     } else if (message.attachments){
-      item.senderId(senderId, {text:'sorry i can not read that...'})
+      item.sendMessage(senderId, {text:'sorry i can not read that...'})
       /*
       // Get the 'type' and the location name
       var location = message.attachments;
